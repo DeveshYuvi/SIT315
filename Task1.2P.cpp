a5324f09fe24bb373860f8d03ab549c55d39750a
DeveshYuvi <88266030+DeveshYuvi@users.noreply.github.com>
2021-08-02 18:51:38 +0400

    Update Task1.2P.cpp

--- Task1.2P.cpp
+++ Task1.2P.cpp
@@ -11,11 +11,12 @@ void setup()
   Serial.begin(9600); //Initialise serial monitor
   pinMode(pir, INPUT);
   pinMode(led, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(sensorPin), pin_ISR, CHANGE); //Attach an interrupt to the ISR vector
+  attachInterrupt(0, pin_ISR, CHANGE); //Attach an interrupt to the ISR vector
 }
 
 void loop()
-{	
+{
+  Serial.println(motion);
   delay(1000);
 }
 
@@ -32,3 +33,4 @@ void pin_ISR(){
     Serial.println("no motion...");
   }
 }  
+
